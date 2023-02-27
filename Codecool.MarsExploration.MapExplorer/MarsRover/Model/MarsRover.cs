5212aea421ccd5e0bbc7a63048adaba446057121
Karjhan <alex_mar1999@yahoo.com>
2023-02-27 12:40:27 +0200

    [alex] Added MarsRover record

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Model/MarsRover.cs
@@ -0,0 +1,5 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+
+public record MarsRover(int id, Coordinate currentPosition, int sight, Dictionary<string, IEnumerable<Coordinate>> foundResources);
