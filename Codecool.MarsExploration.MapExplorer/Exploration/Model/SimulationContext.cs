4a65a20541f5c7d9e8eb1d03e3bbd263750db372
Karjhan <alex_mar1999@yahoo.com>
2023-02-28 11:25:07 +0200

    [alex] finished 2 logger options and context record for simulation

---
+++ Codecool.MarsExploration.MapExplorer/Exploration/Model/SimulationContext.cs
@@ -0,0 +1,6 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Exploration;
+
+public record SimulationContext(int stepsTillTimeout, MarsRover.Model.MarsRover rover, Coordinate spaceshipPosition, Map map, IEnumerable<string> symbols, ExplorationOutcome outcome, int steps = 0);
---
+++ Codecool.MarsExploration.MapExplorer/Logger/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿namespace Codecool.MarsExploration.MapExplorer.Logger;
+
+public class FileLogger : ILogger
+{
+    private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
+    
+    private string outputDir = $"{WorkDir}\\LoggedResults";
+    
+    public void Log(string message)
+    {
+        if (!Directory.Exists(outputDir))
+        {
+            Directory.CreateDirectory(outputDir);
+        }
+        
+        List<string> linesInFile = File.ReadAllLines(outputDir + "\\Results.txt").Skip(1).ToList();
+        
+        linesInFile.Insert(0, "Mars Exploration Results");
+        linesInFile.Insert(1, "\n");
+        
+        linesInFile.Add(DateTime.Now.ToString());
+        linesInFile.Add(message);
+        linesInFile.Add("");
+        
+        File.WriteAllLines(outputDir + "\\Results.txt", linesInFile);
+    }
+}
